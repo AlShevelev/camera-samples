dfe4c3c8099cb7be7d873ab54bba8dc376bc572e
Alex Shevelev <al.e.shevelev@gmail.com>
2021-02-28 20:37:51 +0300

    Tre PreviewView has been removed

--- CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/CameraFragment.kt
+++ CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/CameraFragment.kt
@@ -1,15 +1,12 @@
 package com.android.example.cameraxbasic.fragments
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.hardware.display.DisplayManager
+import android.graphics.SurfaceTexture
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.util.Log
 import android.view.*
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -27,49 +24,25 @@ import kotlin.math.min
  * - Photo taking
  * - Image analysis
  */
-class CameraFragment : Fragment() {
+class CameraFragment : Fragment(), TextureView.SurfaceTextureListener {
 
     private lateinit var container: ConstraintLayout
-    private lateinit var viewFinder: PreviewView
 
     private lateinit var textureView: TextureView
     private lateinit var surface: Surface
 
-    private var displayId: Int = -1
     private var preview: Preview? = null
     private var camera: Camera? = null
     private var cameraProvider: ProcessCameraProvider? = null
 
-    private val displayManager by lazy {
-        requireContext().getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
-    }
-
     /** Blocking camera operations are performed using this executor */
     private lateinit var cameraExecutor: ExecutorService
-
-    /**
-     * We need a display listener for orientation changes that do not trigger a configuration
-     * change, for example if we choose to override config change in manifest or for 180-degree
-     * orientation changes.
-     */
-    private val displayListener = object : DisplayManager.DisplayListener {
-        override fun onDisplayAdded(displayId: Int) = Unit
-        override fun onDisplayRemoved(displayId: Int) = Unit
-        override fun onDisplayChanged(displayId: Int) = view?.let { view ->
-            if (displayId == this@CameraFragment.displayId) {
-                Log.d(TAG, "Rotation changed: ${view.display.rotation}")
-            }
-        } ?: Unit
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
 
         // Shut down our background executor
         cameraExecutor.shutdown()
-
-        // Unregister the broadcast receivers and listeners
-        displayManager.unregisterDisplayListener(displayListener)
     }
 
     override fun onCreateView(
@@ -82,26 +55,12 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         container = view as ConstraintLayout
-        viewFinder = container.findViewById(R.id.view_finder)
-
-        textureView =  container.findViewById(R.id.previewSurface)
-        surface = Surface(textureView.surfaceTexture)
 
         // Initialize our background executor
         cameraExecutor = Executors.newSingleThreadExecutor()
 
-        // Every time the orientation of device changes, update rotation for use cases
-        displayManager.registerDisplayListener(displayListener, null)
-
-        // Wait for the views to be properly laid out
-        viewFinder.post {
-
-            // Keep track of the display in which this view is attached
-            displayId = viewFinder.display.displayId
-
-            // Set up the camera and its use cases
-            setUpCamera()
-        }
+        textureView =  container.findViewById(R.id.previewSurface)
+        textureView.surfaceTextureListener = this
     }
 
     /** Initialize CameraX, and prepare to bind the camera use cases  */
@@ -121,13 +80,11 @@ class CameraFragment : Fragment() {
     private fun bindCameraUseCases() {
 
         // Get screen metrics used to setup camera for full screen resolution
-        val metrics = DisplayMetrics().also { viewFinder.display.getRealMetrics(it) }
-        Log.d(TAG, "Screen metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
 
-        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+        val screenAspectRatio = aspectRatio(textureView.width, textureView.height)
         Log.d(TAG, "Preview aspect ratio: $screenAspectRatio")
 
-        val rotation = viewFinder.display.rotation
+        val rotation = textureView.display.rotation
 
         // CameraProvider
         val cameraProvider = cameraProvider
@@ -183,5 +140,23 @@ class CameraFragment : Fragment() {
         private const val TAG = "CameraXBasic"
         private const val RATIO_4_3_VALUE = 4.0 / 3.0
         private const val RATIO_16_9_VALUE = 16.0 / 9.0
+    }
+
+    override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+        this.surface = Surface(surface)
+
+        // Set up the camera and its use cases
+        setUpCamera()
+    }
+
+    override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {
+    }
+
+    override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+        // Stop rendering
+        return true
+    }
+
+    override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
     }
 }
